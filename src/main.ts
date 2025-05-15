bab316878bfd90d573d85dca0eb051689030bb6c
Victor Momodu <victoramomodu@gmail.com>
2025-05-15 02:48:06 +0100

    feat: setup supertokens

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Kiosk Backend API Specification')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
